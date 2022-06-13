be6dc4210ba71c7fc7dfe94cf9affd6a0cb9d9d6
fredbarre <fredbarremail@protonmail.com>
2022-06-13 10:03:58 +0200

    réécriture du fichier script.js en format plus simplifié

--- front/js/script.js
+++ front/js/script.js
@@ -1,62 +1,38 @@
 let headersList = {
- "Accept": "*/*"
+  Accept: "*/*",
+};
+
+async function getvals() {
+  const response = await fetch("http://localhost:3000/api/products", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  const responseData = await response.json();
+  console.log(responseData);
+  return responseData;
 }
 
-function fetchapi() {
-    
-
-    return fetch("http://localhost:3000/api/products", {
-        method: "GET",
-        headers: headersList
-    }).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log(data);
-    })
-   
+function print1Item(product) {
+  const { id, name, description, imageUrl, altTxt } = product;
+  document.getElementById(
+    "items"
+  ).innerHTML += `<a href="./product.html?id=${id}">
+      <article>
+        <img src="${imageUrl}" alt="${altTxt}">
+        <h3 class="productName">${name}</h3>
+        <p class="productDescription">${description}</p>
+      </article>
+       </a>`;
 }
 
-
-  function getvals(){
-    return fetch("http://localhost:3000/api/products",
-    {
-    	method: "GET",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-    .then((response) => response.json())
-    .then((responseData) => {
-      //console.log(responseData);
-      return responseData;
-    })
-    .catch(error => console.warn(error));
+async function main() {
+  const products = await getvals();
+  for (const product of products) {
+    print1Item(product);
   }
-// getvals().then(response => console.log(response));
-function main() {
-    getvals().then(response =>
-        loopItems(response.length)
-    );
-}
-function loopItems(length) {
-    
-
-for (let i = 0; i < length; i++){
-     getvals().then(response => 
-    print1Item(response[i]._id, response[i].name, response[i].description, response[i].imageUrl, response[i].altText)
-);
-}
-}
-
-
-function print1Item_(){
-    let hello=document.getElementById("items");
-    hello.innerHTML =  "<a href=\"./product.html?id=42\"> <article> <img src=\"../images/kanap01.jpeg\" alt=\"Lorem ipsum dolor sit amet, Kanap name1\"> <h3 class=\"productName\">Kanap name1</h3><p class=\"productDescription\">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p></article></a> ";
-}
-function print1Item(id,name,description,imgurl,alttext){
-    let hello=document.getElementById("items");
-    hello.innerHTML = hello.innerHTML+ "<a href=\"./product.html?id="+id+"\"> <article> <img src=\""+imgurl+"\" alt=\""+alttext+"\"> <h3 class=\"productName\">"+name+"</h3><p class=\"productDescription\">"+description+"</p></article></a> ";
 }
 
 main();
