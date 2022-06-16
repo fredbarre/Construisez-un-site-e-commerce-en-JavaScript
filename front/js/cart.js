6f03209d83064ecbb9af3272c4148a396675b1b8
Yoann Petremann <y.petremann@gmail.com>
2022-06-16 19:51:59 +0200

    Collab 16/06
    Co-authored-by: Stylys <QuentinRousseau@users.noreply.github.com>
    Co-authored-by: fredbarre <fredbarre@users.noreply.github.com>

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,17 @@
-for (let i = 0; i < localStorage.length; i++) {
-  let id = localStorage.key(i);
-  let color = localStorage[id][0];
-  let number = localStorage[id][1];
+import { getCart } from './cartManager.js';
+function main() {
+  const data = getCart();
+  for (let i of data) {
+    printcartitem(i);
+  }
 }
 
-function printcartitem(product, id, color, number) {
-  const { name, imageUrl, altTxt, price } = product;
+function printcartitem(product) {
+  const { name, imageUrl, altTxt, price, _id, color, quantity } = product;
 
-  document.getElementById("cart__items")=`<article class="cart__item" data-id="${id}" data-color="${color}">
+  document.getElementById(
+    'cart__items'
+  ).innerHTML = `<article class="cart__item" data-id="${_id}" data-color="${color}">
                 <div class="cart__item__img">
                   <img src="${imageUrl}" alt="${altTxt}">
                 </div>
@@ -20,7 +24,7 @@ function printcartitem(product, id, color, number) {
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
                       <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${number}">
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
                     </div>
                     <div class="cart__item__content__settings__delete">
                       <p class="deleteItem">Supprimer</p>
