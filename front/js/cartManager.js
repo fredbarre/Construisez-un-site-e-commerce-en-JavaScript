87757daaa87f51d60a2a14318c9ab6bbf099a8dc
fredbarre <fredbarremail@protonmail.com>
2022-06-28 17:02:19 +0200

    Regroupement des produits par id

--- front/js/cartManager.js
+++ front/js/cartManager.js
@@ -13,7 +13,18 @@ export function addToCart(data) {
   );
   if (!item) {
     item = data;
-    cart.push(item);
+    let added = false;
+    for (let i = 0; i < cart.length; i++) {
+      //console.log(cart[i]._id);
+      //console.log(data._id);
+      if (cart[i]._id === data._id) {
+        cart.splice(i, 0, item);
+        added = true;
+        break;
+      }
+    }
+    item = data;
+    if (added == false) cart.push(item);
   } else item.quantity += data.quantity;
   setCart(cart);
   alert("Produit ajouté");
